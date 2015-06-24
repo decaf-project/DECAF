fa65c51d9ff9adaffbc67c4bc34929923e1722eb
tony <tony@tony-Precision-T1700.(none)>
2015-06-24 13:53:15 -0400

    clean the binaries of dalvik instruction tracer plugin

--- DroidScope/qemu/DECAF_plugins/DalvikInstructionTracer/config-plugin.h
+++
@@ -1,6 +0,0 @@
-#define DEFINE_INSN_BEGIN
-#define DEFINE_INSN_END
-#define DEFINE_BLOCK_BEGIN
-#define DEFINE_BLOCK_END
-#define DECAF_HOME "/home/tony/project/android4.3_r2.2/external/qemu_droidscope"
-#define PLUGIN_PATH "/home/tony/project/android4.3_r2.2/external/qemu_droidscope/DECAF_plugins/DalvikInstructionTracer"
