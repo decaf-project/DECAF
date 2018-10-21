a082d3405b7f2e8ad8e9cd48d6d2a5fd0973a06b
mingjun97 <470890875@qq.com>
2018-10-21 16:20:28 -0700

    Clean up the original decaf repo.
    * Move DroidScope into a new branch named DroidScope.
    * Move Docker files into a new branch named docker.

--- DroidScope/qemu/CleanSpec.mk
+++
@@ -1,6 +0,0 @@
-# This empty file is here solely for the purpose of optimizing the Android build
-# Please keep it there, and empty, thanks :-)
-#
-
-$(call add-clean-step, rm -rf $(OUT_DIR)/host/linux-x86/obj/{SHARED,STATIC}_LIBRARIES/emulator*)
-$(call add-clean-step, rm -rf $(OUT_DIR)/host/linux-x86/obj/EXECUTABLES/emulator*)
--- DroidScope/qemu/DECAF_plugins/DalvikInstructionTracer/plugin_cmds.h
+++
@@ -1,7 +0,0 @@
-{
-  .name         = "trace_by_pid",
-  .args_type    = _QEMU_MON_KEY_VALUE("pid","i"),
-  ._QEMU_MON_HANDLER_CMD = do_trace_by_pid,
-  .params       = "[pid]",
-  .help	        = "Start tracing process with PID"
-},
